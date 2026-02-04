4971ebbfbf0d84053e4756d4f570f4bb7db10dd1
Intersected2 <brianx8@nycstudents.net>
2026-02-03 22:39:15 -0500

    done

--- src/Main.java
+++ src/Main.java
@@ -1,70 +1,47 @@
 public class Main {
-    public static void main(String []args){
-        Dice dice1 = new Dice(6);
-        System.out.println("--- CREATING A DICE OBJECT WITH " + dice1.getSides() + " SIDES ---");
-        System.out.println("initial roll count = " + dice1.getRollCount());
-        System.out.println("initial roll value = " + dice1.getRollValue());
-        System.out.println();
-        System.out.println("dice rolled: " + dice1.roll());
-        System.out.println("roll count = " + dice1.getRollCount());
-        System.out.println("most recent roll = " + dice1.getRollValue());
-        System.out.println();
-        System.out.println("dice rolled: " + dice1.roll());
-        System.out.println("roll count = " + dice1.getRollCount());
-        System.out.println("most recent roll = " + dice1.getRollValue());
-        System.out.println();
-        System.out.println("dice rolled: " + dice1.roll());
-        System.out.println("dice rolled: " + dice1.roll());
-        System.out.println("dice rolled: " + dice1.roll());
-        System.out.println("roll count = " + dice1.getRollCount());
-        System.out.println("most recent roll = " + dice1.getRollValue());
-        System.out.println();
-        System.out.println("--- number of sides updated to 20 ---");
-        dice1.setSides(20);
-        System.out.println("dice rolled: " + dice1.roll());
-        System.out.println("roll count = " + dice1.getRollCount());
-        System.out.println("most recent roll = " + dice1.getRollValue());
-        System.out.println();
-        System.out.println("dice rolled: " + dice1.roll());
-        System.out.println("roll count = " + dice1.getRollCount());
-        System.out.println("most recent roll = " + dice1.getRollValue());
-        System.out.println();
-        System.out.println("dice rolled: " + dice1.roll());
-        System.out.println("dice rolled: " + dice1.roll());
-        int roll = dice1.roll();
-        System.out.println("dice rolled: " + roll);
-        System.out.println("roll count = " + dice1.getRollCount());
-        System.out.println("most recent roll = " + dice1.getRollValue());
-        System.out.println();
-        if (roll == dice1.getRollValue()) {
-            System.out.println("PASS");
-        } else {
-            System.out.println("!!!!!! FAIL: most recent roll not being tracked correctly ");
-        }
-        System.out.println();
-        System.out.println("--- CREATING A SECOND DICE OBJECT WITH 10 SIDES ---");
-        Dice dice2 = new Dice(10);
-        System.out.println("dice 2 rolled: " + dice2.roll());
-        System.out.println("dice 2 rolled: " + dice2.roll());
-        System.out.println("dice 2 rolled: " + dice2.roll());
-        System.out.println("dice 2 rolled: " + dice2.roll());
-        System.out.println("dice 2 rolled: " + dice2.roll());
-        System.out.println("dice 2 rolled: " + dice2.roll());
-        System.out.println("dice 2 rolled: " + dice2.roll());
-        roll = dice2.roll();
-        System.out.println("dice 2 rolled: " + roll);
-        System.out.println("dice 2 roll count = " + dice2.getRollCount());
-        System.out.println("dice 1 roll count = " + dice1.getRollCount());
-        System.out.println();
-        if (roll == dice2.getRollValue()) {
-            System.out.println("PASS");
-        } else {
-            System.out.println("!!!!!! FAIL: most recent roll not being tracked correctly ");
-        }
-        if (dice1.getRollCount() == 10 && dice2.getRollCount() == 8) {
-            System.out.println("PASS");
-        } else {
-            System.out.println("!!!!!! FAIL: roll count not being tracked correctly ");
-        }
+    public static void main(String []args) {
+// target goal of 120 minutes of activity
+        ActivityTracker tracker = new ActivityTracker(120);
+        int total = tracker.getTotalMinutes();
+        System.out.println(total);
+        int sessions = tracker.getSessions();
+        System.out.println(sessions);
+        int maxMinTracked = tracker.getMaxMinutes();
+        System.out.println(maxMinTracked);
+        boolean goalMet = tracker.goalAchieved();
+        System.out.println(goalMet);
+        System.out.println(); // blank line
+
+
+        tracker.logMinutes(35);
+        System.out.println(tracker.getTotalMinutes());
+        System.out.println(tracker.getSessions());
+        System.out.println(tracker.getMaxMinutes());
+        System.out.println(tracker.goalAchieved());
+        System.out.println(); // blank line
+
+
+        tracker.logMinutes(30);
+        System.out.println(tracker.getTotalMinutes());
+        System.out.println(tracker.getSessions());
+        System.out.println(tracker.getMaxMinutes());
+        System.out.println(tracker.goalAchieved());
+        System.out.println(); // blank line
+
+
+        tracker.logMinutes(45);
+        System.out.println(tracker.getTotalMinutes());
+        System.out.println(tracker.getSessions());
+        System.out.println(tracker.getMaxMinutes());
+        System.out.println(tracker.goalAchieved());
+        System.out.println(); // blank line
+
+
+        tracker.logMinutes(15);
+        System.out.println(tracker.getTotalMinutes());
+        System.out.println(tracker.getSessions());
+        System.out.println(tracker.getMaxMinutes());
+        System.out.println(tracker.goalAchieved());
+
     }
 }
--- src/Prism.java
+++ src/Prism.java
@@ -2,6 +2,10 @@ public class Prism {
     private int length = 0;
     private int width = 0;
     private int height = 0;
+    private int ram = 0;
+    private int ram1 = 0;
+    private int ram2 = 0;
+    private int sa = 0;
     public Prism(int length, int width, int height){
         this.length = length;
         this.width = width;
@@ -12,13 +16,26 @@ public class Prism {
         this.width = sidelength;
         this.height = sidelength;
     }
-    public int returnDimensions(){
+    public String dimensions(){
         return "Length: " + this.length + "\n" + "Width: " + this.width + "\n" + "Height: " + this.height;
     }
     public int volume(){
         return this.height * this.width * this.length;
     }
     public int surfaceArea(){
-
+        ram = this.length * this.width * 2;
+        ram1 = this.length * this.height * 2;
+        ram2 = this. width * this.height * 2;
+        sa = ram + ram1 + ram2;
+        return sa;
+    }
+    public void setLength(int length){
+        this.length = length;
+    }
+    public void setWidth(int width){
+        this.width = width;
+    }
+    public void setHeight(int height){
+        this.height = height;
     }
 }
